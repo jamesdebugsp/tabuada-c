43c2fee3e95b99dff186d4225f14870d3e079e7e
James Oliveira <james.jhey020@icloud.com>
2025-05-01 16:49:05 -0300

    Projeto em C - tabuada

---
+++ tabuada.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    double largura, comprimento, valor, area, preco;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimento);
+
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valor);
+
+    area = largura * comprimento;
+    preco = area * valor;
+
+    printf("Area do terreno = %.2lf\n", area);
+    printf("Preco do terreno = %.2lf\n", preco);
+
+    getchar(); // lê o '\n' que ficou no buffer
+    getchar(); // espera você apertar uma tecla antes de fechar
+
+    return 0;
+}
+
